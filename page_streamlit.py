89e2a5611a6f76d48e69a4efc81fa26c776ccf15
Mugule <124181984+Mugule@users.noreply.github.com>
2025-10-01 11:28:09 +0200

    Update page_streamlit.py

--- page_streamlit.py
+++ page_streamlit.py
@@ -8,4 +8,6 @@ equip_numero = "E001I850190001"
 img = qrcode.make(url + equip_numero)
 img_pil = img.get_image() if hasattr(img, "get_image") else img
 
+st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+st.write(label, unsafe_allow_html=False)
 st.image(img_pil, caption="QR code", use_container_width=True)

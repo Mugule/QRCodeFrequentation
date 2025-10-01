9278c3374d058eb989bd9386a77b618880961480
Mugule <124181984+Mugule@users.noreply.github.com>
2025-10-01 11:36:06 +0200

    Update page_streamlit.py

--- page_streamlit.py
+++ page_streamlit.py
@@ -7,6 +7,7 @@ equip_numero = "E001I850190001"
 
 st.text_input("Numéro d'équipement", value=equip_numero)
 
-img = qrcode.make(url + equip_numero)
-img_pil = img.get_image() if hasattr(img, "get_image") else img
-st.image(img_pil, caption="QR code", use_container_width=True)
+if st.button("Go QR Code"):
+   img = qrcode.make(url + equip_numero)
+   img_pil = img.get_image() if hasattr(img, "get_image") else img
+   st.image(img_pil, caption="QR code", use_container_width=True)

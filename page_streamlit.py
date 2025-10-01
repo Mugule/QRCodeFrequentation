034f8cad5e71596006bc7ce3c08ba0e21511590c
Mugule <124181984+Mugule@users.noreply.github.com>
2025-10-01 17:07:30 +0200

    Update page_streamlit.py

--- page_streamlit.py
+++ page_streamlit.py
@@ -4,25 +4,25 @@ import requests
 
 url = "https://equipements-sportsgouv.contribuer.io/fr/tables/P5m4Puz4NDnxvxgtODuq2gY2L_bkGbVbmKN4HphSjyE/contributions/new?prefill_equip_numero="
 
+# Si requete
 if len(st.query_params) > 0 :
-    numero = st.query_params["numero"]
-else :
-    numero = "E001I850190001"
-
-equip_numero = st.text_input("Numéro d'équipement", numero)
-
-request = "https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?select=inst_nom%2C%20equip_nom%2C%20equip_type_name&where=equip_numero%20%3D%20%27" + equip_numero +"%27&limit=1"
-
-data = requests.get(request).json()
-
-if len(data["results"]) > 0 :
-    installation = data["results"][0]['inst_nom']
-    equipement = data["results"][0]['equip_nom']
-    type = data["results"][0]['equip_type_name']
-    st.write(installation + ' - ' + equipement + " (" + type +")")
+    equip_numero = st.query_params["numero"]
+    # equip_numero = st.text_input("Numéro d'équipement", numero)
+    request = "https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?select=inst_nom%2C%20equip_nom%2C%20equip_type_name&where=equip_numero%20%3D%20%27" + equip_numero +"%27&limit=1"
+    data = requests.get(request).json()
     
-    img = qrcode.make(url + equip_numero)
-    img_pil = img.get_image() if hasattr(img, "get_image") else img
-    st.image(img_pil, caption="QR code", width=400)
+    if len(data["results"]) > 0 :
+        installation = data["results"][0]['inst_nom']
+        equipement = data["results"][0]['equip_nom']
+        type = data["results"][0]['equip_type_name']
+        st.write(installation + ' - ' + equipement + " (" + type +")")
+        
+        img = qrcode.make(url + equip_numero)
+        img_pil = img.get_image() if hasattr(img, "get_image") else img
+        st.image(img_pil, caption="QR code", width=400)
+    else :
+        st.write("Numéro équipement invalide")
+# Si pas de requete
 else :
-    st.write("Numéro équipement invalide")
+    st.write("Indiquez une requête valide pour la création d'un QR code")
+

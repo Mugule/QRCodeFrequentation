6e0388f1fe7e0fbf2b1fdb5afe38f98b360c0fc8
Mugule <124181984+Mugule@users.noreply.github.com>
2025-10-01 11:24:32 +0200

    Update page_streamlit.py

--- page_streamlit.py
+++ page_streamlit.py
@@ -6,5 +6,6 @@ url = "https://equipements-sportsgouv.contribuer.io/fr/tables/P5m4Puz4NDnxvxgtOD
 equip_numero = "E001I850190001"
 
 img = qrcode.make(url + equip_numero)
+img_pil = img.get_image() if hasattr(img, "get_image") else img
 
-st.image(img)
+st.image(img_pil, caption="QR code", use_container_width=True)

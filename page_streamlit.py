de6c2712d273373d54290227695de60fb547191c
Mugule <124181984+Mugule@users.noreply.github.com>
2025-10-01 11:50:04 +0200

    Update page_streamlit.py

--- page_streamlit.py
+++ page_streamlit.py
@@ -9,8 +9,15 @@ equip_numero = st.text_input("Numéro d'équipement", "E001I850190001")
 request = "https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?select=inst_nom%2C%20equip_nom%2C%20equip_type_name&where=equip_numero%20%3D%20%27" + equip_numero +"%27&limit=1"
 
 data = requests.get(request).json()
-st.write(data)
 
-img = qrcode.make(url + equip_numero)
-img_pil = img.get_image() if hasattr(img, "get_image") else img
-st.image(img_pil, caption="QR code", width=400)
+if len(data["results"]) > 0 :
+    installation = data["results"][0]['inst_nom']
+    equipement = data["results"][0]['equip_nom']
+    type = data["results"][0]['equip_type_name']
+    st.write(installation + ' - ' + equipement + " (" + type +")")
+    
+    img = qrcode.make(url + equip_numero)
+    img_pil = img.get_image() if hasattr(img, "get_image") else img
+    st.image(img_pil, caption="QR code", width=400)
+else :
+    st.write("Numéro équipement invalide")

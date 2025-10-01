9873e998c59e99a67117034b2a37d09b98541a55
Mugule <124181984+Mugule@users.noreply.github.com>
2025-10-01 11:38:17 +0200

    Update page_streamlit.py

--- page_streamlit.py
+++ page_streamlit.py
@@ -3,11 +3,8 @@ import qrcode
 
 url = "https://equipements-sportsgouv.contribuer.io/fr/tables/P5m4Puz4NDnxvxgtODuq2gY2L_bkGbVbmKN4HphSjyE/contributions/new?prefill_equip_numero="
 
-equip_numero = "E001I850190001"
+equip_numero = st.text_input("Numéro d'équipement", "E001I850190001")
 
-st.text_input("Numéro d'équipement", value=equip_numero)
-
-if st.button("Go QR Code"):
-   img = qrcode.make(url + equip_numero)
-   img_pil = img.get_image() if hasattr(img, "get_image") else img
-   st.image(img_pil, caption="QR code", use_container_width=True)
+img = qrcode.make(url + equip_numero)
+img_pil = img.get_image() if hasattr(img, "get_image") else img
+st.image(img_pil, caption="QR code", use_container_width=True)
